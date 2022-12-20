a636ace225c4c92755a43e15cd21ed3f89999931
trimonovds <trimonovds@yandex-team.ru>
2022-12-20 17:17:05 +0600

    Split setup a bit

---
+++ lua/trimonovds/options.lua
@@ -0,0 +1,31 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath "cache" .. "/undo"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 250
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.termguicolors = true
+vim.opt.showmode = false -- lualine already shows mode
+
