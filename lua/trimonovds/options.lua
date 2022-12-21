79484270055c6b8415d1e7d85e1f4e58eb09ae27
trimonovds <trimonovds@yandex-team.ru>
2022-12-21 15:03:04 +0600

    Add netrw setup

--- lua/trimonovds/options.lua
+++ lua/trimonovds/options.lua
@@ -29,3 +29,7 @@ vim.opt.splitright = true
 vim.opt.termguicolors = true
 vim.opt.showmode = false -- lualine already shows mode
 
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 1
+vim.g.netrw_winsize = 25
