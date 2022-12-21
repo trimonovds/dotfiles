baf97a4cb8198d86ba5df1b8edcec81a22bfcf1f
trimonovds <trimonovds@yandex-team.ru>
2022-12-21 15:55:20 +0600

    Make pagination better

--- lua/trimonovds/keymaps.lua
+++ lua/trimonovds/keymaps.lua
@@ -1,6 +1,11 @@
 -- Mappings.
 vim.g.mapleader = " "
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
