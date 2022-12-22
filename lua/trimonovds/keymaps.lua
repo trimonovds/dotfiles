5b565a6a197b38e9ab4322cba4033770e58c7130
trimonovds <trimonovds@yandex-team.ru>
2022-12-23 03:42:04 +0600

    Set better diagnostic keymaps

--- lua/trimonovds/keymaps.lua
+++ lua/trimonovds/keymaps.lua
@@ -64,10 +64,10 @@ vim.keymap.set("x", "<A-k>", ":move '<-2<CR>gv-gv")
 -- Lsp
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, opts)
 
 -- NvimTree
 vim.keymap.set("n", "<leader>e", "<cmd>:NvimTreeToggle<cr>")
