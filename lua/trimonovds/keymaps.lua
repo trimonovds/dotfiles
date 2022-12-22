a778f98287bd2cc237e593952b8bdcd068d70a0c
trimonovds <trimonovds@yandex-team.ru>
2022-12-23 04:49:58 +0600

    Remove windown navigation keymaps

--- lua/trimonovds/keymaps.lua
+++ lua/trimonovds/keymaps.lua
@@ -19,12 +19,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-
 -- Resize with arrows
 vim.keymap.set("n", "<C-Up>", ":resize +2<CR>")
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>")
