a4fde5e143da724ab45a6a693d09f5f43bb10278
trimonovds <trimonovds@yandex-team.ru>
2022-12-24 19:58:26 +0600

    Fix sumneko_lua config

--- lua/trimonovds/lsp.lua
+++ lua/trimonovds/lsp.lua
@@ -44,8 +44,17 @@ require('lspconfig')['sumneko_lua'].setup {
   settings = {
     Lua = {
       diagnostics = {
-        globals = { 'vim' }
-      }
-    }
-  }
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
