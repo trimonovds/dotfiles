2cd055c761a509ab03d88d466cf2bfba91c70f30
trimonovds <trimonovds@yandex-team.ru>
2023-01-02 17:36:21 +0300

    Update colorcolumn value to 120

--- lua/trimonovds/options.lua
+++ lua/trimonovds/options.lua
@@ -2,7 +2,7 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = '120'
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
