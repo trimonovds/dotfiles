99b83a1160a727ed3bee3dc7fa643420fb20570a
trimonovds <trimonovds@gmail.com>
2023-01-06 03:27:45 +0300

    NvimTree cfg improved a bit

--- lua/trimonovds/plugin-config/nvim-tree.lua
+++ lua/trimonovds/plugin-config/nvim-tree.lua
@@ -1,7 +1,9 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 require("nvim-tree").setup({
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+    ignore_list = {},
+  },
   view = {
     adaptive_size = true,
     mappings = {
